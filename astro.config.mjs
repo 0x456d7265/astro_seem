aa01e55d18ebde2643e8d8675fe27484b5e0af61
Emre ÇELİK <emrecelikbu@gmail.com>
2025-01-10 21:29:57 +0300

    Add client-side routing support and 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://0x456d7265.github.io',
-  base: '/astro_seem'
+  base: '/astro_seem',
+  output: 'static',
+  build: {
+    assets: '_astro'
+  },
+  trailingSlash: 'always'
 });
